6ce32b6a68439e00531b300fb4c036dd978cbc19
joker <le_yemeng@163.com>
2024-03-04 16:04:11 +0800

    feat: commitlint

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/404">About</router-link>
-  </div>
-  <router-view />
+	<div id="nav">
+		<router-link to="/">Home</router-link> |
+		<router-link to="/404">About</router-link>
+	</div>
+	<router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+	}
 
-#nav {
-  padding: 30px;
-  // background: url($src + "/id/729/200/300.jpg?hmac=VbcZBxFYzQK1ro1MTLLmwHNQ0kuIJSagOeue4JMymUY")
-  //   no-repeat;
-  // background: url("~@/assets/logo.png") no-repeat center;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+	#nav {
+		padding: 30px;
+		// background: url($src + "/id/729/200/300.jpg?hmac=VbcZBxFYzQK1ro1MTLLmwHNQ0kuIJSagOeue4JMymUY")
+		//   no-repeat;
+		// background: url("~@/assets/logo.png") no-repeat center;
+		a {
+			font-weight: bold;
+			color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+			&.router-link-exact-active {
+				color: #42b983;
+			}
+		}
+	}
 </style>
