ffe0387046753c69432a03a32772056e50406fc8
joker <lee_yemeng@163.com>
2024-03-05 17:02:38 +0800

    fix: 更改邮箱

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/404">About222</router-link>
+    <router-link to="/404">About22s2</router-link>
   </div>
   <router-view />
 </template>
