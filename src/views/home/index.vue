ebc54923da969c8d7778cb50f681c054244a486c
joker <le_yemeng@163.com>
2024-03-05 14:47:34 +0800

    fix: test lint

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,7 +4,7 @@
 <script setup>
   const a = 1;
   const b = 'sss';
-  const obj = { a: 1, b: 2, c: 4 };
-  const sss = '12433';
+
+  console.log(a, b);
 </script>
 <style scoped lang="scss"></style>
